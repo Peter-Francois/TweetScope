6c3395a2e1d8869749845b9a49a347fdcafc62f2
Peter Francois <peter.francois237@hotmail.com>
2025-04-15 08:12:40 +0200

    feat(types): creat Tweet type and use it in app

---
+++ frontend/src/lib/types/tweet.ts
@@ -0,0 +1,7 @@
+export interface Tweet {
+    id: number;
+    author: string;
+    content: string;
+    date: Date;
+    theme: string;
+  }
--- frontend/src/routes/tweets/+page.ts
+++ frontend/src/routes/tweets/+page.ts
@@ -1,19 +1,19 @@
 import type { PageLoad } from './$types';
+import type { Tweet } from '$lib/types/tweet';
 
-export const load: PageLoad = async ({ fetch }) => {
+
+export const load: PageLoad = async ({ fetch }): Promise<{ tweets: Tweet[] }> => {
   try {
     const res = await fetch('http://localhost:8000/tweets');
     if (!res.ok) {
-      // Lancez une erreur ici pour que SvelteKit puisse la gérer
+      
       throw new Error(`Failed to fetch tweets: ${res.statusText}`);
     }
-    const tweets = await res.json();
-    // Le console.log ici s'exécutera côté serveur ou lors du fetch côté client
-    // console.log('Tweets loaded:', tweets);
-    return { tweets: tweets || [] }; // Assurez-vous de retourner un tableau
+    const tweets: Tweet[] = await res.json();
+    return { tweets };
   } catch (error) {
     console.error('Error loading tweets:', error);
-    // Vous pouvez aussi retourner un objet d'erreur spécifique
-    return { tweets: [], error: 'Could not load tweets.' };
+
+    return { tweets: [] };
   }
 };
